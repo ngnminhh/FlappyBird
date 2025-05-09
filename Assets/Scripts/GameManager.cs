33b6bd465c0ec66e523c6c9c0af4fa3f72c9020d
PHONEFARMPC\phonefarm <minionrush203@gmail.com>
2025-05-09 17:36:31 +0700

    +Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{   
+    public Player player;
+    public Text scoreText;
+    public GameObject Play_BTN;
+    public GameObject gameOver;
+
+    private int score;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        Play_BTN.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+        player.transform.position = Vector3.zero;
+        typeof(Player).GetField("direction", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(player, Vector3.zero);
+
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false; 
+
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        Play_BTN.SetActive(true);
+        Pause();    
+        Debug.Log("Game over");
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }    
+}
---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    
+    public float speed = 5f;
+    
+
+    private float leftEdge;
+    private bool hasScored = false;
+
+
+    private void Start()
+    {
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+        
+    }
+
+    private void Update()
+    {
+        transform.position += speed * Time.deltaTime * Vector3.left;
+
+        if (transform.position.x < leftEdge)
+        {
+            Destroy(gameObject);
+    
+        }
+
+        if (!hasScored && transform.position.x < GameObject.FindGameObjectWithTag("Player").transform.position.x)
+        {
+            FindObjectOfType<GameManager>().IncreaseScore();
+            hasScored = true;
+        }
+
+    }
+
+
+
+}
